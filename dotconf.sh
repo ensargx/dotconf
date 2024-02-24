9cd2434000d3cb32ae9361294f0c2aaa160b2f7a
Ensar Gök <ensarxgok@gmail.com>
2024-02-24 09:26:48 +0300

    chore: cp rf

--- dotconf.sh
+++ dotconf.sh
@@ -8,62 +8,24 @@ fi
 usr_conf=$usr_home/.config
 
 files=(
-    "$usr_conf/nvim/"
+    "$usr_conf/nvim"
 )
-
-check_file() {
-    if [[ -f $1 ]]; then
-        return 0
-    fi
-    if [[ -d $1 ]]; then
-        return 1
-    fi
-    return -1
-}
 
 pull() {
     for file in "${files[@]}"; do
-        check_file "$file"
-        case $? in
-            0)
-                echo "Pull File: $file" 
-                ;;
-            1)
-                ts=${file%/}
-                tn=${ts##*/}
-                echo "Running: cp -rf $ts $tn"
-                cp -rf $ts $tn
-                ;;
-            -1)
-                echo "Error: The file or directory does not exist."
-                echo "File: $file"
-                exit 1
-                ;;
-        esac
+        echo "Pulling dotfiles from the files array."
+        fc="${file##*/}"
+        fn="${file%/*}"
+        echo "fn: $fn"
+        echo "fc: $fc"
+        echo "running: cp -rf $fn/$fc $fc"
     done
 }
 
 push() {
     echo "Pushing dotfiles to the files array."
     for file in "${files[@]}"; do
-        check_file "$file"
-        case $? in
-            0)
-                echo "Push File: $file"
-                ;;
-            1)
-                ts=${file%/}
-                tn=${ts##*/}
-                ts="${ts%/*}/"
-                echo "Running: cp -rf $tn $ts"
-                cp -rf $tn $ts
-                ;;
-            -1)
-                echo "Error: The file or directory does not exist."
-                echo "File: $file"
-                exit 1
-                ;;
-        esac
+        echo "Pushing dotfiles to the files array."
     done
 }
 
