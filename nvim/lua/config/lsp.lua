13d5f8dc57a50a5f0035739ccb759e6aab21225d
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2025-12-08 23:55:15 +0300

    better comp

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -2,10 +2,6 @@ vim.lsp.enable('lua_ls')
 vim.lsp.enable('rust_analyzer')
 vim.lsp.enable('clangd')
 
--- LSP completion sırasında TAB ile seçim yapmak
-vim.api.nvim_set_keymap('i', '<Tab>', [[pumvisible() ? "\<C-y>" : "\<Tab>"]], {expr = true, noremap = true})
-vim.api.nvim_set_keymap('s', '<Tab>', [[pumvisible() ? "\<C-y>" : "\<Tab>"]], {expr = true, noremap = true})
-
 vim.keymap.set('n', 'de', vim.diagnostic.open_float)
 vim.keymap.set('n', 'db', vim.diagnostic.goto_prev)
 vim.keymap.set('n', 'dn', vim.diagnostic.goto_next)
@@ -14,18 +10,18 @@ vim.keymap.set('n', 'dq', vim.diagnostic.setloclist)
 -- Inlay hints
 vim.lsp.inlay_hint.enable(true)
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
-      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-      vim.keymap.set('i', '<C-Space>', function()
-        vim.lsp.completion.get()
-      end)
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   callback = function(ev)
+--     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+--     if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+--       vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+--       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+--       vim.keymap.set('i', '<C-Space>', function()
+--         vim.lsp.completion.get()
+--       end)
+--     end
+--   end,
+-- })
 
 -- Diagnostics
 vim.diagnostic.config({
--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -34,7 +34,26 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+  completion = {
+    accept = {
+      -- experimental auto-brackets support
+      auto_brackets = {
+        enabled = true,
+      },
+    },
+    menu = {
+      draw = {
+        treesitter = { "lsp" },
+      },
+    },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 200,
+      window = {
+        border = 'single',
+      }
+    },
+  },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
